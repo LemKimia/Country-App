759b6321892adecb1386be7c2c36d3b53d8c13ca
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-07-08 10:42:59 +0700

    feat: adding api response data types

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,5 +1,5 @@
-
 import axios from "axios";
+import {Country} from "./api-type";
 
 export const getNation = async () => {
     const url = "https://restcountries.com/v3.1/all"
@@ -7,7 +7,7 @@ export const getNation = async () => {
     try {
         const response = await axios.get(url)
 
-        return response.data
+        return response.data as Country[]
     } catch (error: any) {
         throw Error(error.response.data.message)
     }
