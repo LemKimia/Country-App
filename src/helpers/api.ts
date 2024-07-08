13efe6df8f5f8c96802c5c208935267e5eec776d
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-07-08 18:04:41 +0700

    chores: changing api names

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {Country} from "./api-type";
 
-export const getNation = async () => {
+export const getCountryData = async () => {
     const url = "https://restcountries.com/v3.1/all"
 
     try {
