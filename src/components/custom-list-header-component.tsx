8d77bce120204be1b17413483ed480e338420798
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-07-09 15:21:28 +0700

    feat: adding dropdown feature to list the continent category necessary for filtering

--- src/components/custom-list-header-component.tsx
+++ src/components/custom-list-header-component.tsx
@@ -1,20 +1,64 @@
-import {View, TextInput, StyleSheet} from "react-native";
+import {View, Text, TextInput, StyleSheet} from "react-native";
+import SelectDropdown from 'react-native-select-dropdown'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 interface Props {
-    keyword: string
-    setKeyword: (text: string) => void;
+    keywordForName: string
+    keywordForContinent: string
+    setKeywordForName: (text: string) => void;
+    setKeywordForContinent: (text: string) => void;
 }
 
 const CustomListHeaderComponent = (props: Props) => {
-    const {keyword, setKeyword} = props
+    const {keywordForName, keywordForContinent, setKeywordForName, setKeywordForContinent} = props
+    const countryContinentOptions = [
+        'africa',
+        'antarctica',
+        'asia',
+        'europe',
+        'north america',
+        'oceania',
+        'south america'
+    ]
 
     return (
         <View style={style.container}>
-            <TextInput
-                onChangeText={setKeyword}
-                placeholder={'Search by Continent'}
-                value={keyword}
-            />
+            <View style={style.textInputContainer}>
+                <TextInput
+                    onChangeText={setKeywordForName}
+                    placeholder={'Search by Common Name'}
+                    value={keywordForName}
+                />
+            </View>
+            <View style={style.dropdownContainer}>
+                <SelectDropdown
+                    data={countryContinentOptions}
+                    onSelect={(selectedItem) => {
+                        setKeywordForContinent(selectedItem)
+                    }}
+                    renderButton={(selectedItem, isOpen) => {
+                        return (
+                            <View>
+                                <Text>{selectedItem || 'Search by Continent'}</Text>
+                            </View>
+                        )
+                    }}
+                    renderItem={(item, index, isSelected) => {
+                        return (
+                            <View>
+                                <Text>{item}</Text>
+                            </View>
+                        )
+                    }}
+                    // search
+                    // searchInputTxtColor={'#151E26'}
+                    // searchPlaceHolder={'Search here'}
+                    // searchPlaceHolderColor={'#72808D'}
+                    // renderSearchInputLeftIcon={() => {
+                    //     return <FontAwesome name={'search'} color={'#72808D'} size={18}/>;
+                    // }}
+                />
+            </View>
         </View>
     )
 }
@@ -23,7 +67,18 @@ export default CustomListHeaderComponent
 
 const style = StyleSheet.create({
     container: {
-        width: 300,
+        flexDirection: "row"
+    },
+    textInputContainer: {
+        width: 175,
+        padding: 2,
+        borderWidth: 2,
+        borderRadius: 8,
+        borderColor: "#C4DFDF",
+        backgroundColor: "#D2E9E9",
+    },
+    dropdownContainer: {
+        width: 150,
         padding: 2,
         borderWidth: 2,
         borderRadius: 8,
