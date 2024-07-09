7a7b16a9eb485a2ff32fd846bf3c6c791d964348
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-07-09 15:23:40 +0700

    feat: adding filtering by continent and by name, or both in the same time. adding function to reset the parameter to filter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,36 +12,62 @@ import CustomListHeaderComponent from "../components/custom-list-header-componen
 export default function Homepage() {
     const [data, setData] = useState<Country[]>([])
     const [showList, setShowList] = useState(true)
-    const [keyword, setKeyword] = useState('')
-    const buttonLabel = showList ? "Show Country" : "Close List"
+    const [keywordForName, setKeywordForName] = useState('')
+    const [keywordForContinent, setKeywordForContinent] = useState('')
+    const resetLabel = keywordForContinent || keywordForName ? "Reset List" : "Close List"
+    const buttonLabel = showList ? "Show Country" : resetLabel
 
     const getCountry = async () => {
         try {
             const response = await getCountryData()
 
             setData(response)
-
         } catch (error: any) {
             alert(error.message)
         }
     }
+
     const handlePress = async () => {
-        setShowList((prevState) => !prevState)
+        if (keywordForName || keywordForContinent) {
+            setKeywordForName((prevState) => "")
+            setKeywordForContinent((prevState) => "")
+        } else {
+            setShowList((prevState) => !prevState)
+        }
     }
 
-    const filteredCountryData = keyword === '' ? data : data.filter(country => (
-        country.continents.toString().toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+    const filterCountryByContinent = keywordForContinent === '' ? data : data.filter(country => (
+        country.continents.toString().toLocaleLowerCase().includes(keywordForContinent.toLocaleLowerCase())
     ))
+
+    const filterCountryByName = keywordForName === '' ? data : data.filter(country => (
+        country.name.common.toString().toLocaleLowerCase().includes(keywordForName.toLocaleLowerCase())
+    ))
+
+    const filterCountryByContinentAndName = data.filter(country =>
+        country.continents.toString().toLocaleLowerCase().includes(keywordForContinent.toLocaleLowerCase()) &&
+        country.name.common.toString().toLocaleLowerCase().includes(keywordForName.toLocaleLowerCase())
+    )
+
+    const filteredCountryData = keywordForContinent && keywordForName ? filterCountryByContinentAndName
+        : keywordForContinent ? filterCountryByContinent
+            : keywordForName ? filterCountryByName
+                : data
 
     useEffect(() => {
         getCountry()
-
     }, [])
 
     return (
         <View style={styles.container}>
             <Button handlePress={handlePress} buttonLabel={buttonLabel}/>
-            {!showList && <CustomListHeaderComponent keyword={keyword} setKeyword={setKeyword}/>}
+            {!showList &&
+                <CustomListHeaderComponent
+                    keywordForName={keywordForName}
+                    keywordForContinent={keywordForContinent}
+                    setKeywordForName={setKeywordForName}
+                    setKeywordForContinent={setKeywordForContinent}
+                />}
             <View style={showList ? {height: "auto"} : styles.listContainer}>
                 {!showList && (
                     <FlatList
@@ -49,7 +75,7 @@ export default function Homepage() {
                         renderItem={({item}) => (
                             <CountryCard country={item}/>
                         )}
-                        keyExtractor={(item) => item.name.official}
+                        keyExtractor={(item) => item.name.common}
                         key={2}
                         numColumns={2}
                     />
