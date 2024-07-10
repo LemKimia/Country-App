01b9b19bee84c3392f97d0cddbd87e6b61fb62eb
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-07-10 10:48:32 +0700

    feat: added new api to get country detail with name as the parameter

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -12,3 +12,15 @@ export const getCountryData = async () => {
         throw Error(error.response.data.message)
     }
 };
+
+export const getCountryDetail = async (name: string) => {
+    const url = `https://restcountries.com/v3.1/${name}`
+
+    try {
+        const response = await axios.get(url)
+
+        return response.data as Country[]
+    } catch (error: any) {
+        throw Error(error.response.data.message)
+    }
+}
